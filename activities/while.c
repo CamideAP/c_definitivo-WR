8a6f98c4e563372b2d2d9020aaf1a62d97ea4ed0
camideAP <andrarte91@gmail.com>
2022-04-14 09:22:40 -0300

    nested while

--- activities/while.c
+++ activities/while.c
@@ -1,8 +1,30 @@
+/*==================================================================================================
+	Title: while command
+	Autor: Camila de A. Pires
+	Data: 14/04/2022
+	Descrição: Module 05, part 1 from Linguagem C (Wagner Rambo) Course
+==================================================================================================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+main() {
+	/*Nested while: Multiplication table*/
+	int tab,n;
+	
+	tab =1;
+	while(tab<=10){
+		n=1;
+		while(n<=10){
+			printf("%2d x %2d = %3d\n",tab,n,n*tab);
+			n++;
+		}
+		printf("\n");
+		tab++;
+	}
+	
+	system("PAUSE");
 	return 0;
 }
