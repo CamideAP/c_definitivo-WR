2bae834f02a3a81256c8e9c42d2d3344d7a06397
camideAP <andrarte91@gmail.com>
2022-04-16 10:38:25 -0300

    finishes the do while class

--- activities/intermediate_1/mainDoWhile.c
+++ activities/intermediate_1/mainDoWhile.c
@@ -1,8 +1,15 @@
+/*==================================================================================================
+	Title: do while command
+	Author: Camila de A. Pires
+	Date: 16/04/2022
+	Description: Module 05, class 2 from Linguagem C (Wagner Rambo) Course
+==================================================================================================*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h> /*Graphic accentuation library*/
 
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+/* do {} while(); executes one time the instructions before testing the condition */
 
 int main(int argc, char *argv[]) {
 	
@@ -27,6 +34,61 @@ int main(int argc, char *argv[]) {
 			default: printf("invalid item\n\n");
 		}
 	} while (opt!='q' && opt!='Q');
+	putchar('\n');
+	
+	/*---		Testing condition of inputs		---*/
+	
+	int number;
+	
+	do{
+		printf("Type a number: ");
+		scanf("%d", &number);
+	} while(number>=10); /* asks for a user input while the number is greater than or equal 10
+	 */
+	 
+	system("CLS");
+	
+	/*---		Challenge:
+	Develop a system with options to subscribe a client. You have to use the following options: N - New client, S - Search clients, D - Delete client,
+	C - Clear the clients list, Q - quit.
+	The input should consider lowercase and uppercase letters, the output shows just the options you've choosen, nothing else.
+	---*/
+	char subs;
+	
+	do{
+		printf("N - New Client.\n");
+		printf("S - Search Client.\n");
+		printf("D - Delete Client.\n");
+		printf("C - Clear the clients list.\n");
+		printf("Q - Quit.\n");
+		printf("Option: ");
+		scanf(" %c",&subs);
+		system("CLS");
+		
+		switch(subs)
+		{
+			case 'N': 
+			case 'n': puts("New Client"); break; /* puts - prints a string (we can't use variables here), adds a new line in the final*/
+			case 'S':
+			case 's': puts("Search Client"); break;
+			case 'D':
+			case 'd': puts("Delete Client"); break;
+			case 'C':
+			case 'c': puts("Clear the Clients list"); break;
+			case 'Q':
+			case 'q': puts("Quit"); break;
+			default: printf("invalid item\n");
+		}
+		putchar('\n');
+	} while (subs!='q' && subs!='Q');
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	system("PAUSE");
 	return 0;
