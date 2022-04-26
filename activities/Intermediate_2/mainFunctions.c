1df229ed315b4e8e446419b2dfaa958536631f17
camideAP <andrarte91@gmail.com>
2022-04-26 14:54:17 -0300

    Adds function lesson

---
+++ activities/Intermediate_2/mainFunctions.c
@@ -0,0 +1,18 @@
+/*==================================================================================================
+	Title: Functions
+	Author: Camila de A. Pires
+	Date: 26/04/2022
+	Description: Module 06, lesson 0 from Linguagem C (Wagner Rambo) Course
+==================================================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+main() {
+	
+	
+	
+	return 0;
+}
