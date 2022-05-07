4fded6f7f35bf48091b050feb1d5cacec482d205
camideAP <andrarte91@gmail.com>
2022-05-07 13:58:03 -0300

    finishes the function parameters lesson

--- activities/Intermediate_2/mainParameterFunction.c
+++ activities/Intermediate_2/mainParameterFunction.c
@@ -8,9 +8,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void specialChar(int a);
-void freq_555(float Ra,float Rb, float C);
 
+/*==================================================================================================*/
+/* --- Prototypes --- */
+void specialChar(int a),
+	 freq_555(float Ra,float Rb, float C), 
+	 lowPassFilter(float res,float cap, char selection);
+	 /*We can also write the prototypes in this way when they return the same kind of variable */
+
+/*==================================================================================================*/
+/* --- Main function --- */
 int main() {
 	int GFUser = 1;
 	
@@ -23,6 +30,8 @@ int main() {
 	float a,b,c;
 	
 	while(sw){
+		printf("Let's calculate the output frequency from the 555 oscillator:\n\n");
+		
 		printf("Type Ra value: ");
 		scanf("%f", &a);
 		printf("Type Rb value: ");
@@ -32,14 +41,52 @@ int main() {
 		freq_555(a,b,c);
 		printf("Type 1 to continue and Zero to quit: ");
 		scanf("%d", &sw);
-		if (sw){
-			system("CLS");
+		if (sw) system("CLS");
+	}
+	
+	printf("\n\n");
+		
+	float re,ca;
+	char select = 'a';
+		
+	while(select!='0'){
+		printf("Let's calculate the frequency or time of a low pass filter:\n\n");
+	
+		printf("Type Resistor value: ");
+		scanf("%f", &re);
+		printf("Type Capacitor value: ");
+		scanf("%f", &ca);
+		
+		printf("Type f for frequency, t for time: ");
+		scanf("	%c", &select);	
+		while(select!='f'&&select!='t'&&select!='0'){
+			printf("\n\n Wrong valuer! Type f for frequency, t for time: ");	
+			scanf(" %c", &select);				
 		}
 		
+		if (select=='f'||select=='t') lowPassFilter(re , ca, select);
+		
+		printf("\nDo you want to calculate again? (Y/N) ");
+		scanf(" %c", &sw);
+		switch(sw){
+			case 'Y':
+			case 'y': 	system("CLS");
+						break;
+			case 'N':
+			case 'n': select = '0'; break;
+			default: 	system("CLS");
+						printf("\nYou chose a different value, so let's calculate again!\n\n'");
+		}				
 	}
+	
+	system("CLS");
+	printf("We finished all tests here. See you again! :-)\n\n");
 	
 	return 0;
 }
+
+/*==================================================================================================*/
+/* --- Functions --- */
 
 void specialChar(int a){
 	int i;
@@ -75,6 +122,19 @@ void specialChar(int a){
 void freq_555(float Ra,float Rb, float C){
 	float freq;
 	
-	freq = 1,44 /((Ra+Rb)*C);
+	freq = 1.44 /((Ra+2*Rb)*C);
 	printf("\nThe 555 frequency is %.2fHz\n\n", freq);
+}
+
+void lowPassFilter(float res,float cap, char selection){
+	float result;
+	
+	if (selection=='f'){
+		result = 1/(2*3.1416*res*cap);
+		printf("\nf = %.2fHz\n\n", result);
+	}
+	else{
+		result = res*cap;
+		printf("\nt = %fs\n\n", result);
+	}
 }
